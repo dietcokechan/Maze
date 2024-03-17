19ff2890f9ab4bb47b1317a2e107036967c654fd
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-03-17 23:40:19 +0200

    opens a window using sdl2

---
+++ inc/maze.h
@@ -0,0 +1,17 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+/*standard io*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/*sdl*/
+#include <SDL.h>
+
+/*screen dimensions*/
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+#endif /* MAZE_H */
